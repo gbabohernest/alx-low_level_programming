dde4efc43eba18ba6e0e6c0de5d5dcf7dedc3e26
Ernest <gbaboh.ernest@gmail.com>
2023-02-17 02:32:22 +0000

    using puts to print to std output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,8 @@
 */
 int main(void)
 {
-	char string[] = "Programming is like building a multilingual puzzle";
+	char string[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(string);
 	return (0);
 }
