c8bf2991d8ff3600b1ca984109f3ca49610b3f9a
Ernest <gbaboh.ernest@gmail.com>
2023-02-20 23:57:29 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
 * main - Entry Point.
 *
@@ -6,6 +7,6 @@
 */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
