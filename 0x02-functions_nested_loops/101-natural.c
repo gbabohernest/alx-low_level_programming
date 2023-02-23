2dedf1978ace2c850808eb6804a813208e09c09a
Ernest <gbaboh.ernest@gmail.com>
2023-02-23 02:35:55 +0000

    natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,22 +3,21 @@
 * main - Entry point
 * Return: 0 (Success)
 */
+
 int main(void)
 {
-	int i;
-	int sum;
+	int i, sum = 0;
 
-	sum = 0;
-
-	for (i = 0; i < 10; i++)
+	while (i < 1024)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-			sum = sum + i;
+			sum += i;
 		}
+
 		i++;
 	}
+
 	printf("%d\n", sum);
 	return (0);
 }
-
