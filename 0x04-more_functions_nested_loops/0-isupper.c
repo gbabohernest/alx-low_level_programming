5047bed25cfe58f000d9ddffde52687b99cab2b1
Ernest <gbaboh.ernest@gmail.com>
2023-02-24 10:37:15 +0000

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*_isupper - function that checks for uppercase character.
+*@c: integer parameter
+*Return: 1 if c is uppercase, 0 otherwise
+*/
+
+int _isupper(int c)
+{
+	int result;
+
+	if (c >= 'A' && c >= 'Z')
+	{
+		result = 1;
+	}
+	else
+	{
+		result = 0;
+	}
+	return (result);
+}
