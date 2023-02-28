e8b4b6f01aea236c7d835aced04dd49de7f5e5f7
Ernest <gbaboh.ernest@gmail.com>
2023-02-28 21:15:13 +0000

    fix betty doc style

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,14 +10,6 @@ void puts2(char *str)
 {
 	int i;
 
-	/*while (str[i] != '\0')
-	{
-		if (i % 2 == 0)
-		_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n'); */
-
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i % 2 == 0)
