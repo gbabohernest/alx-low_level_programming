e24bb1f53ddfe12580662a7288d36130098835f1
Ernest <gbaboh.ernest@gmail.com>
2023-03-06 23:12:47 +0000

    locates first occurrence of a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*_strchr -locates a character in a string
+*
+*@s: string to be searched
+*@c: string to be located
+*
+*Return:If c is found - a pointer to the first occurence.
+*       If c is not found - NULL.
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	return ('\0');
+}
