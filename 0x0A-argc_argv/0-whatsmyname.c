991cf96825200f1cebddbc41ecf179d2bbb645a7
Ernest <gbaboh.ernest@gmail.com>
2023-03-15 23:34:01 +0000

    solution for task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - Prints the name of the program, followed by a new line
+
+*@argc:number of arguments supplied to the program
+*@argv:an array of pointers to the arguments.
+*
+*Return: 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
