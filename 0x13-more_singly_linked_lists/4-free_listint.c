a827c36c1ff2f63334392b9d9d18d9f8369998ec
Ernest <gbaboh.ernest@gmail.com>
2023-04-04 04:19:33 +0000

    free_listint function frees the memory of all nodes been use

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+* free_listint - Fress the memory occupied by the signly linked list
+* @head: A pointer to the first node in the list
+* Return: nothing (void)
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	temp = head;
+
+	if (head == NULL)
+	{
+		exit(0);
+	}
+
+	while (temp != NULL)
+	{
+		head = temp;
+		temp = temp->next;
+		free(head);
+	}
+}
