9ee252a56d4eedfc08af74fbbc9fa381401152a8
Ernest <gbaboh.ernest@gmail.com>
2023-04-05 00:31:28 +0000

    Return the sum of all node date(n) in the list, 0 if the list is empty

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+* sum_listint - Return the sum of all data(n) of a linked list
+* @head: A pointer to the first node in the list
+* Return: Total amount of data, 0 if list is empty
+*/
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *tempNode;
+
+	tempNode = head;
+	if(tempNode == NULL)
+	{
+		return (0);
+	}
+
+	while (tempNode != NULL)
+	{
+		sum += tempNode->n;
+		tempNode = tempNode->next;
+	}
+	return (sum);
+}
