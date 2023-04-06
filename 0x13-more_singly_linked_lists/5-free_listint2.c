1cfbf643ccb2dae89f0e3fcbaddb6a1fecf27546
Ernest <gbaboh.ernest@gmail.com>
2023-04-06 02:53:00 +0000

    bug fix as regard to the NULL case

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,20 +10,19 @@
 void free_listint2(listint_t **head)
 {
 	int i;
-	listint_t *tmpNode = *head;
+	listint_t *tmpNode;
 	listint_t *tmp_next;
 
 	if (head == NULL)
 	{
 		return;
 	}
+	tmpNode = (*head);
 	for (i = 0; tmpNode != NULL; i++)
 	{
 		tmp_next = tmpNode;
 		tmpNode = tmpNode->next;
-		/*tmp_next = t->next;*/
 		free(tmp_next);
-		/*t = tmp_next;*/
 	}
 	*head = NULL;
 }
