4c97db7a13b61cf244c2765cd2246b1e0c5d8c97
Ernest <gbaboh.ernest@gmail.com>
2023-05-08 16:55:28 +0000

    add a solution for edge case 3

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,23 +10,24 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *tmp_node, *tmp2_node;
-	size_t i;
+	size_t i = 0;
 
 	tmp_node = head;
 
 	if (tmp_node == NULL)
 		exit(98);
 
-	for (i = 0; tmp_node != NULL; i++)
+	while (tmp_node)
 	{
 		printf("[%p] %d\n", (void *) tmp_node, tmp_node->n);
+		i++;
 		tmp2_node = tmp_node;
 		tmp_node = tmp_node->next;
 
 		if (tmp2_node <= tmp_node)
 		{
 			printf("-> [%p] %d\n", (void *) tmp_node, tmp_node->n);
-			/*exit(98);*/
+			return (i);
 		}
 	}
 	return (i);
