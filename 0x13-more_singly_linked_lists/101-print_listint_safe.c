f8ef56e32cb4c55cd64084da2dcd303529c3d247
Ernest <gbaboh.ernest@gmail.com>
2023-05-08 17:03:56 +0000

    add solution for correct output edge cases for when head is NULL and nodes alignment in memory

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,8 +9,8 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *tmp_node, *tmp2_node;
 	size_t i = 0;
+	const listint_t *tmp_node, *tmp2_node;
 
 	tmp_node = head;
 
@@ -27,7 +27,7 @@ size_t print_listint_safe(const listint_t *head)
 		if (tmp2_node <= tmp_node)
 		{
 			printf("-> [%p] %d\n", (void *) tmp_node, tmp_node->n);
-			return (i);
+			return (98);
 		}
 	}
 	return (i);
