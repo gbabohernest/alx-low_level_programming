34214c6f7aa2a547f6cb09699d811885c304c3a4
Ernest <gbaboh.ernest@gmail.com>
2023-05-20 01:43:45 +0000

    add code to handle eage cases 3,5,7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -58,12 +58,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				new_node->prev = tmp;
 				new_node->next = tmp->next;
 				tmp->next = new_node;
-				new_node->next->prev = new_node;
+				if (new_node->next != NULL)
+					new_node->next->prev = new_node;
 				return (new_node);
 			}
 			tmp = tmp->next;
 			i++;
 		}
 	}
+	free(new_node);
 	return (NULL);
 }
