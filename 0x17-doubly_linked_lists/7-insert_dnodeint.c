53a4ed5bad691fda4678effcfd0ebb354a41975d
Ernest <gbaboh.ernest@gmail.com>
2023-05-20 23:59:53 +0000

    add fun to add at the end of the list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -45,6 +45,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new_node = add_dnodeint(h, n);
 	}
+	if (idx == length)
+	{
+		new_node = add_dnodeint_end(h, n);
+	}
 
 	tmp = *h;
 	while (tmp != NULL)
