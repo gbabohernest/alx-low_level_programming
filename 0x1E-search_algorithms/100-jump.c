2e52cd57ccd5a4bce8df1e2c8194c4c9a0325838
Ernest <gbaboh.ernest@gmail.com>
2023-12-14 01:06:50 +0000

    remove size check

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -17,7 +17,7 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t jump_size, start, end, i;
 
-	if (array == NULL || size == 0)
+	if (array == NULL)
 		return (-1);
 
 	jump_size = sqrt(size);
